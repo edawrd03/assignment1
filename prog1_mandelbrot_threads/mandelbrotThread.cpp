601cef5c209f95324a48144d7c692797a853de64
edward003 <1406221784@qq.com>
2021-09-06 21:50:34 +0800

    finish prog1

--- prog1_mandelbrot_threads/mandelbrotThread.cpp
+++ prog1_mandelbrot_threads/mandelbrotThread.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <stdio.h>
 #include <pthread.h>
 
@@ -6,8 +7,8 @@
 typedef struct {
     float x0, x1;
     float y0, y1;
-    unsigned int width;
-    unsigned int height;
+    int width;  // avoid overflow hence change to singned type
+    int height;
     int maxIterations;
     int* output;
     int threadId;
@@ -22,6 +23,12 @@ extern void mandelbrotSerial(
     int maxIterations,
     int output[]);
 
+extern void mandelbrotSerialInterleave(
+    float x0, float y0, float x1, float y1,
+    int width, int height,
+    int threadId, int threadNum,
+    int maxIterations,
+    int output[]);
 
 //
 // workerThreadStart --
@@ -29,12 +36,25 @@ extern void mandelbrotSerial(
 // Thread entrypoint.
 void* workerThreadStart(void* threadArgs) {
 
+    double startTime = CycleTimer::currentSeconds();
     WorkerArgs* args = static_cast<WorkerArgs*>(threadArgs);
 
-    // TODO: Implement worker thread here.
+    // block spatial decomposition
 
-    printf("Hello world from thread %d\n", args->threadId);
+    // int numRows = (args->height + 1) / args->numThreads;
+    // int startRow = args->threadId * numRows;
+    // int totalRows = numRows + startRow > args->height ? args->height - startRow : numRows;
 
+    // mandelbrotSerial(args->x0, args->y0, args->x1, args->y1, args->width, args->height,
+    //                  startRow, totalRows, args->maxIterations, args->output);
+
+    // interleave spatial decomposition
+
+    mandelbrotSerialInterleave(args->x0, args->y0, args->x1, args->y1, args->width, args->height,
+                     args->threadId, args->numThreads, args->maxIterations, args->output);
+
+    double endTime = CycleTimer::currentSeconds();
+    printf("[worker thread %d]:\t\t[%.3f] ms\n", args->threadId, std::min(1e30, endTime - startTime) * 1000);
     return NULL;
 }
 
@@ -62,6 +82,15 @@ void mandelbrotThread(
 
     for (int i=0; i<numThreads; i++) {
         // TODO: Set thread arguments here.
+        args[i].x0 = x0;
+        args[i].x1 = x1;
+        args[i].y0 = y0;
+        args[i].y1 = y1;
+        args[i].width = width;
+        args[i].height = height;
+        args[i].numThreads = numThreads;
+        args[i].maxIterations = maxIterations;
+        args[i].output = output;
         args[i].threadId = i;
     }
 
